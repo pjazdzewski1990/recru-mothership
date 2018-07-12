23ff1e68c79f9b23531a660f510dbd98effc3d16
Patryk <pjazdzewski1990@gmail.com>
2018-07-12 23:24:01 +0200

    add mising service tests

---
+++ src/test/scala/io/scalac/recru/GameServiceSpec.scala
@@ -0,0 +1,57 @@
+package io.scalac.recru
+
+import akka.actor.ActorSystem
+import akka.testkit.{TestKit, TestProbe}
+import io.scalac.recru.GameService.{GameJoined, Moved, WrongTurn}
+import io.scalac.recru.Model._
+import io.scalac.recru.Signals.SignalListenLocation
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, MustMatchers}
+import org.scalatest.concurrent.{Eventually, ScalaFutures}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class GameServiceSpec extends TestKit(ActorSystem("GameActor"))
+  with FlatSpecLike with MustMatchers with BeforeAndAfterAll with Eventually with ScalaFutures {
+
+  val p1 = Player("p1")
+  val listenLocation = SignalListenLocation("loc")
+
+  "GameService" should "search and find a game" in {
+    val gameId = GameId("black-jack")
+
+    val probe = TestProbe()
+
+    val service = new ActorGameService(probe.ref)
+    val result = service.searchForAGame(p1)
+
+    probe.expectMsg(GameManagerActor.FindGameForPlayer(p1))
+    probe.reply(GameManagerActor.GameFound(gameId, listenLocation, Red))
+    result.futureValue mustBe GameJoined(gameId, listenLocation, Red)
+  }
+
+  it should "pass down and react to valid moves" in {
+    val gameId = GameId("foo-bar")
+
+    val probe = TestProbe()
+
+    val service = new ActorGameService(probe.ref)
+    val result = service.move(gameId, p1, Blue, ForwardOneField)
+
+    probe.expectMsg( GameManagerActor.MakeAMove(gameId, p1, Blue, ForwardOneField))
+    probe.reply(GameManagerActor.Moved)
+    result.futureValue mustBe Moved
+  }
+
+  it should "pass down  and act on invalid moves" in{
+    val gameId = GameId("bar-baz")
+
+    val probe = TestProbe()
+
+    val service = new ActorGameService(probe.ref)
+    val result = service.move(gameId, p1, Blue, ForwardOneField)
+
+    probe.expectMsg( GameManagerActor.MakeAMove(gameId, p1, Blue, ForwardOneField))
+    probe.reply(GameManagerActor.NotYourTurn)
+    result.futureValue mustBe WrongTurn
+  }
+}
