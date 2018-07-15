9afb5f89a30b27bdc6f35e867a9750a30c9408be
Patryk <pjazdzewski1990@gmail.com>
2018-07-15 15:11:15 +0200

    make sure correct messages are sent over to kafka

--- src/main/scala/io/scalac/recru/messaging/KafkaSignals.scala
+++ src/main/scala/io/scalac/recru/messaging/KafkaSignals.scala
@@ -1,15 +1,18 @@
 package io.scalac.recru.messaging
 
 import akka.Done
-import akka.kafka.ProducerSettings
+import akka.kafka.{ProducerMessage, ProducerSettings}
 import akka.kafka.scaladsl.Producer
-import akka.stream.{Materializer}
-import akka.stream.scaladsl.Source
+import akka.stream.{Materializer, OverflowStrategy, QueueOfferResult}
+import akka.stream.scaladsl.{Keep, Sink, Source}
 import com.typesafe.config.ConfigFactory
 import io.scalac.recru.Model.{Color, GameId, Move, Player}
 import io.scalac.recru.messaging.Signals.SignalListenLocation
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.StringSerializer
+import spray.json.{JsArray, JsNumber, JsObject, JsString, JsValue}
+
+import scala.concurrent.Future
 
 class KafkaSink(implicit val mat: Materializer) {
 
@@ -20,39 +23,102 @@ class KafkaSink(implicit val mat: Materializer) {
     ProducerSettings(config, new StringSerializer, new StringSerializer)
       .withBootstrapServers("127.0.0.1:9092")
 
-  //TODO: ...
-  def foo() = {
-    import scala.concurrent.ExecutionContext.Implicits.global
-    val f = Source(1 to 10)
-      .map(_.toString)
-      .map(value => new ProducerRecord[String, String](listenLocation.v, value))
-      .runWith(Producer.plainSink(producerSettings))
-    f.onComplete(r => println(s"~~~ ${r}"))
-    f
+  val bufferSize = 1000
+
+  val publishToKafka = Source.queue[JsValue](bufferSize, OverflowStrategy.backpressure)
+    .map(_.prettyPrint) // we print pretty to make debugging easier
+    .map{ v =>
+      //TODO: the "game-all" key should be replaced with a gameId, this way we could have strong order per game not per all messages
+      ProducerMessage.Message(
+        new ProducerRecord[String, String](listenLocation.v, "game-all", v),
+        1
+      )
+    }
+    .via(Producer.flexiFlow(producerSettings))
+    .toMat(Sink.ignore)(Keep.left)
+    .run()
+
+  def pushToKafka(evt: JsValue): Future[Boolean] = {
+    import mat.executionContext
+
+    publishToKafka.offer(evt).map {
+      case QueueOfferResult.Enqueued =>
+        true
+      case QueueOfferResult.Dropped | QueueOfferResult.QueueClosed =>
+        false
+      case QueueOfferResult.Failure(ex) =>
+        println(s"Offer failed ${ex.getMessage}")
+        false
+    }
   }
 
 }
 
+private[messaging] object KafkaMarshalling {
+  def marshallGameStart(gameId: GameId, players: Set[Player]): JsValue = {
+    val playersV = players.map(p => JsString(p.name)).toVector
+    JsObject(
+      ("type", JsString("game-start")),
+      ("gameId", JsString(gameId.v)),
+      ("players", JsArray(playersV))
+    )
+  }
+
+  def marshallTurn(gameId: GameId, playerMovingNow: Player): JsValue = {
+    JsObject(
+      ("type", JsString("game-new-turn")),
+      ("gameId", JsString(gameId.v)),
+      ("player", JsString(playerMovingNow.name))
+    )
+  }
+
+  def marshallGameUpdate(gameId: GameId, player: Player, movedColor: Color, move: Move): JsValue = {
+    JsObject(
+      ("type", JsString("game-update")),
+      ("gameId", JsString(gameId.v)),
+      ("player", JsString(player.name)),
+      ("color", JsString(movedColor.toString)),
+      ("move", JsNumber(move.moveValue))
+    )
+  }
+
+  def marshallGameEnd(gameId: GameId, winners: Seq[Player], losers: Seq[Player]): JsValue = {
+    val winnersV = winners.map(p => JsString(p.name)).toVector
+    val losersV = losers.map(p => JsString(p.name)).toVector
+    JsObject(
+      ("type", JsString("game-end")),
+      ("gameId", JsString(gameId.v)),
+      ("winners", JsArray(winnersV)),
+      ("losers", JsArray(losersV))
+    )
+  }
+}
+
 class KafkaSignals(sink: KafkaSink) extends Signals {
+  import KafkaMarshalling._
   // Note: Signals now support a multi-tenant mode only (1 stream, many games), but could easily be moved to single tenant
   override val listenLocation = sink.listenLocation
 
   override def signalGameStart(gameId: GameId, players: Set[Player]): Done = {
-    sink.foo()
+    //TODO: return type of push* is disconnected from the signal* methods, this could lead to kafka signals gone "missing" in case of backpressure
+    sink.pushToKafka(marshallGameStart(gameId, players))
     println(s"Started a game ${gameId} with ${players}")
     Done
   }
   override def signalTurn(gameId: GameId, playerMovingNow: Player): Done = {
+    sink.pushToKafka(marshallTurn(gameId, playerMovingNow))
     println(s"Game ${gameId} now waits on move from ${playerMovingNow}")
     Done
   }
   // Note: it would be a more useful design to include the whole state of board in the message, but it's intentionally made harder
   override def signalGameUpdate(gameId: GameId, player: Player, movedColor: Color, move: Move): Done = {
+    sink.pushToKafka(marshallGameUpdate(gameId, player, movedColor, move))
     println(s"Updated game ${gameId} with ${player}, ${move} for ${movedColor}")
     Done
   }
 
   override def signalGameEnd(gameId: GameId, winners: Seq[Player], losers: Seq[Player]): Done = {
+    sink.pushToKafka(marshallGameEnd(gameId, winners, losers))
     println(s"Game ${gameId} ended ${winners} losers ${losers}")
     Done
   }
